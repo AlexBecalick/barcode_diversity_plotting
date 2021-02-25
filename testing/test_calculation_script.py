7d48a90a18d9c6e1f89fff26a1cd649f48afdfa0
AlexBecalick <alexander.becalick@crick.ac.uk>
2021-02-25 13:29:11 +0000

    Reverted  import syntax

--- testing/test_calculation_script.py
+++ testing/test_calculation_script.py
@@ -4,7 +4,7 @@ Unit tests for the calculation function.
 import pytest
 import sys
 import numpy
-import diversity_plotting.diversity_calculation_funcs as df
+from diversity_plotting import diversity_calculation_funcs as df
 
 
 def test_barcode_array_generation():
