21d465d434c27dc78d7c701fd6659da7ccdc7a28
AlexBecalick <alexander.becalick@crick.ac.uk>
2021-02-25 10:35:27 +0000

    Added pytest

---
+++ test_calculation_script.py
@@ -0,0 +1,9 @@
+"""
+Unit tests for the calculation function
+"""
+
+import pytest
+import diversity_calculation_funcs
+
+def test_give_answer():
+    assert diversity_calculation_funcs.my_data(1, 100, 1, 0)[] == [1, 1]
