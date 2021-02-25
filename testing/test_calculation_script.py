24832703b8f9e3f6ed4d137aa6b030359e7dc1f4
AlexBecalick <alexander.becalick@crick.ac.uk>
2021-02-25 12:30:05 +0000

    Test for barcode array generation

--- testing/test_calculation_script.py
+++ testing/test_calculation_script.py
@@ -3,7 +3,19 @@ Unit tests for the calculation function.
 """
 
 import pytest
-import diversity_calculation_funcs
+import diversity_calculation_funcs as df
 
-def test_give_answer():
-    assert diversity_calculation_funcs.my_data(1, 100, 1, 0)[] == [1, 1]
+def test_barcode_array_generation():
+    expected = [[ 1.          1.01005017]
+    [ 2.          1.02020134]
+    [ 3.          1.03045453]
+    [ 4.          1.04081077]
+    [ 5.          1.0512711 ]
+    [ 6.          1.06183655]
+    [ 7.          1.07250818]
+    [ 8.          1.08328707]
+    [ 9.          1.09417428]
+    [10.          1.10517092]]
+    actual = list(df.my_data(10, 100, 1, 0))
+    
+    assert actual == expected
