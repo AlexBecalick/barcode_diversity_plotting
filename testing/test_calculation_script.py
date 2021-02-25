a0bacd9bca7be2ec375df0392813f07c6a9a1df3
AlexBecalick <alexander.becalick@crick.ac.uk>
2021-02-25 12:52:30 +0000

    Edited testing with relative values

--- testing/test_calculation_script.py
+++ testing/test_calculation_script.py
@@ -3,19 +3,20 @@ Unit tests for the calculation function.
 """
 
 import pytest
+import numpy
 import diversity_calculation_funcs as df
 
 def test_barcode_array_generation():
-    expected = [[ 1.          1.01005017]
-    [ 2.          1.02020134]
-    [ 3.          1.03045453]
-    [ 4.          1.04081077]
-    [ 5.          1.0512711 ]
-    [ 6.          1.06183655]
-    [ 7.          1.07250818]
-    [ 8.          1.08328707]
-    [ 9.          1.09417428]
-    [10.          1.10517092]]
-    actual = list(df.my_data(10, 100, 1, 0))
+    expected = np.array([[ 1.        ,  1.01005017],
+       [ 2.        ,  1.02020134],
+       [ 3.        ,  1.03045453],
+       [ 4.        ,  1.04081077],
+       [ 5.        ,  1.0512711 ],
+       [ 6.        ,  1.06183655],
+       [ 7.        ,  1.07250818],
+       [ 8.        ,  1.08328707],
+       [ 9.        ,  1.09417428],
+       [10.        ,  1.10517092]])
+    actual = list(df.my_data(10, 100, 1, 1))
     
-    assert actual == expected
+    np.testing.assert_allclose(expected, actual, rtol=1e-06)
